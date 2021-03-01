0152429d69b9e8b462669448844958476628345a
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-03-01 22:00:40 +0700

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -1,12 +1,12 @@
-#include <errno.h> 
+#include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 
 #define DEFAULT_FILE_NAME "file"
 
-
-FILE* open_file(const char* file_name) {
+FILE* open_file(const char* file_name)
+{
     FILE* file_for_return = fopen(file_name, "r");
     if (file_for_return == NULL) {
         fprintf(stderr, "Can't open %s file.\n", file_name);
@@ -15,25 +15,26 @@ FILE* open_file(const char* file_name) {
     return file_for_return;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     FILE* input_file;
 
-   const char* file_name = (argc == 2) ? argv[1] : DEFAULT_FILE_NAME;
+    const char* file_name = (argc == 2) ? argv[1] : DEFAULT_FILE_NAME;
 
     for (int index = 0; index < 2; ++index) {
-       
+
         fprintf(stdout, "Real UID:%u\nEffective UID:%u\n", getuid(), geteuid());
         input_file = open_file(file_name);
         if (input_file == NULL) {
-            return (EXIT_FAILURE);//выход из фун-и main с аргументом EXIT_FAILURE(макрос, указывающий на состояние выполнения программы)
+            return (EXIT_FAILURE);
         }
-      if (setuid(getuid()) < 0) {
+        if (setuid(getuid()) < 0) {
             fputs("Can't change euid\n", stderr);
             perror(argv[0]);
             return (EXIT_FAILURE);
         }
 
- if (fclose(input_file) == EOF) {
+        if (fclose(input_file) == EOF) {
             fprintf(stderr, "Can't close %s\n", file_name);
             perror(argv[0]);
             return (EXIT_FAILURE);
@@ -42,4 +43,3 @@ int main(int argc, char* argv[]) {
 
     return (EXIT_SUCCESS);
 }
-
