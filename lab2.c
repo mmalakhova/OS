8376e2b75ae1ea954daae1e773743f5385d5f037
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-03-01 22:06:09 +0700

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -2,26 +2,26 @@
 #include <sys/types.h>
 #include <time.h>
 #include <stdio.h>
-extern char *tzname[];
+extern char* tzname[];
 
 int main(void)
 {
     time_t now;
-    struct tm *sp;
-    if ( putenv("TZ=PST8PDT")){
+    struct tm* sp;
+    if (putenv("TZ=PST8PDT")) {
         fprintf(stderr, "Can't change TZ variable.\n");
         perror("putenv");
     }
-    
+
     (void)time(&now);
 
     printf("%s", ctime(&now));
 
     sp = localtime(&now);
     printf("%d/%d/%02d %d:%02d %s\n",
-           sp->tm_mon + 1, sp->tm_mday, sp->tm_year ,
-           sp->tm_hour , sp->tm_min,
-           tzname[sp->tm_isdst]);
+        sp->tm_mon + 1, sp->tm_mday, sp->tm_year,
+        sp->tm_hour, sp->tm_min,
+        tzname[sp->tm_isdst]);
 
     return 0;
 }
