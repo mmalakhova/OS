b830b3881d750792ee5aefc924cec5790a4d6c28
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-03-02 09:03:15 +0700

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -1,20 +1,3 @@
-#include <errno.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-
-#define DEFAULT_FILE_NAME "file"
-
-FILE* open_file(const char* file_name)
-{
-    FILE* file_for_return = fopen(file_name, "r");
-    if (file_for_return == NULL) {
-        fprintf(stderr, "Can't open %s file.\n", file_name);
-        perror("fopen");
-    }
-    return file_for_return;
-}
-
 int main(int argc, char* argv[])
 {
     FILE* input_file;
@@ -26,17 +9,16 @@ int main(int argc, char* argv[])
         fprintf(stdout, "Real UID:%u\nEffective UID:%u\n", getuid(), geteuid());
         input_file = open_file(file_name);
         if (input_file == NULL) {
-            return (EXIT_FAILURE);
+            return (EXIT_FAILURE); 
         }
+
         if (setuid(getuid()) < 0) {
-            fputs("Can't change euid\n", stderr);
-            perror(argv[0]);
+            perror("Error");
             return (EXIT_FAILURE);
         }
 
         if (fclose(input_file) == EOF) {
-            fprintf(stderr, "Can't close %s\n", file_name);
-            perror(argv[0]);
+            perror("Error");
             return (EXIT_FAILURE);
         }
     }
