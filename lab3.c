aeb3dcfd7cd2927efad1db97bb31fc568b27803a
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-03-04 17:08:57 +0700

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -1,5 +1,20 @@
-int main(int argc, char* argv[])
-{
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define DEFAULT_FILE_NAME "file"
+#define ERROR -1
+
+FILE* open_file(const char* file_name) {
+    FILE* file_for_return = fopen(file_name, "r");
+    if (file_for_return == NULL) {
+        perror("Error");
+    }
+    return file_for_return;
+}
+
+int main(int argc, char* argv[]) {
     FILE* input_file;
 
     const char* file_name = (argc == 2) ? argv[1] : DEFAULT_FILE_NAME;
@@ -9,10 +24,11 @@ int main(int argc, char* argv[])
         fprintf(stdout, "Real UID:%u\nEffective UID:%u\n", getuid(), geteuid());
         input_file = open_file(file_name);
         if (input_file == NULL) {
-            return (EXIT_FAILURE); 
+            return (EXIT_FAILURE);
         }
 
-        if (setuid(getuid()) < 0) {
+        int err = setuid(geteuid());
+        if (err == ERROR) {
             perror("Error");
             return (EXIT_FAILURE);
         }
