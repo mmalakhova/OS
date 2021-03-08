af0bc14e2af5100896503f7adf4b9570960ea0d8
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-03-09 01:03:28 +0700

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -6,7 +6,8 @@
 #define DEFAULT_FILE_NAME "file"
 #define ERROR -1
 
-FILE* open_file(const char* file_name) {
+FILE* open_file(const char* file_name)
+{
     FILE* file_for_return = fopen(file_name, "r");
     if (file_for_return == NULL) {
         perror("Error");
@@ -14,29 +15,39 @@ FILE* open_file(const char* file_name) {
     return file_for_return;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     FILE* input_file;
 
     const char* file_name = (argc == 2) ? argv[1] : DEFAULT_FILE_NAME;
 
-    for (int index = 0; index < 2; ++index) {
+    fprintf(stdout, "Real UID:%u\nEffective UID:%u\n", getuid(), geteuid());
+    input_file = open_file(file_name);
 
-        fprintf(stdout, "Real UID:%u\nEffective UID:%u\n", getuid(), geteuid());
-        input_file = open_file(file_name);
-        if (input_file == NULL) {
-            return (EXIT_FAILURE);
-        }
+    if (input_file == NULL) {
+        return (EXIT_FAILURE);
+    }
+    if (fclose(input_file) == EOF) {
+        perror("Error");
+        return (EXIT_FAILURE);
+    }
 
-        int err = setuid(geteuid());
-        if (err == ERROR) {
-            perror("Error");
-            return (EXIT_FAILURE);
-        }
+    int err = setuid(geteuid());
 
-        if (fclose(input_file) == EOF) {
-            perror("Error");
-            return (EXIT_FAILURE);
-        }
+    if (err == ERROR) {
+        perror("Error");
+        return (EXIT_FAILURE);
+    }
+    fprintf(stdout, "Real UID:%u\nEffective UID:%u\n", getuid(), geteuid());
+    input_file = open_file(file_name);
+
+    if (input_file == NULL) {
+        return (EXIT_FAILURE);
+    }
+
+    if (fclose(input_file) == EOF) {
+        perror("Error");
+        return (EXIT_FAILURE);
     }
 
     return (EXIT_SUCCESS);
