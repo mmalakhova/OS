58089cec18c91da42e8c967800b2e49a7b0ee444
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-03-13 17:09:30 +0700

    Update lab5.c

--- lab5.c
+++ lab5.c
@@ -1,52 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h> // error codes
+#include <errno.h>     // error codes
 #include <sys/types.h> // lseek, open
-#include <sys/stat.h> // open
-#include <fcntl.h> // open
-#include <unistd.h> // lseek
+#include <sys/stat.h>  // open
+#include <fcntl.h>     // open
+#include <unistd.h>    // lseek
 
 #define LINES_COUNT_MAX 100
 #define END_LINE_NUMBER 0
 #define ERROR -1
 #define SUCCESS 0
+#define DEFAULT_FILE_NAME "text"
 
-// INFO: использовать STDIN_FILENO, STDOUT_FILENO и STDERR_FILENO вместо магических 0, 1, 2
 
-/**
- *
- * return: lines count
- */
-int BuildLinesOffsetsTable(int fileDescriptor, long* linesOffsets, int* linesLengths)
+ //return: lines count
+
+int BuildLinesOffsetsTable(int fileDescriptor, long *linesOffsets, int *linesLengths)
 {
     int currentLine = 1;
     int currentPosInLine = 0;
 
     linesOffsets[1] = 0L;
 
-    while (1) {
+    while (1)
+    {
         char currentChar;
         int readReturnCode = read(fileDescriptor, &currentChar, 1);
 
         // On error, -1 is returned, and errno is set appropriately.  In this case, it is left unspecified whether the file position (if any) changes.
-        if (readReturnCode == ERROR) {
+        if (readReturnCode == ERROR)
+        {
             perror("Reading Error: ");
             exit(EXIT_FAILURE);
         }
 
         // On  success, the number of bytes read is returned (zero indicates end of file), and the file position is advanced by this number.
-        if (readReturnCode == SUCCESS) { // EOF
+        if (readReturnCode == SUCCESS)
+        { // EOF
             break;
         }
 
-        if (currentLine > LINES_COUNT_MAX + 1) {
+        if (currentLine > LINES_COUNT_MAX + 1)
+        {
             fprintf(stderr, "Error: file have more than %d lines\n", LINES_COUNT_MAX);
             exit(EXIT_FAILURE);
         }
 
         currentPosInLine++;
 
-        if (currentChar != '\n') {
+        if (currentChar != '\n')
+        {
             continue;
         }
 
@@ -55,9 +58,10 @@ int BuildLinesOffsetsTable(int fileDescriptor, long* linesOffsets, int* linesLen
 
         // Upon successful completion, lseek() returns the resulting offset
         // location as measured in bytes from the beginning of the file.
-        int currentLineOffset = lseek(fileDescriptor, 0L, 1);
+        int currentLineOffset = lseek(fileDescriptor, 0L, SEEK_CUR);
 
-        if (currentLineOffset == ERROR) { // the value (off_t) -1 is returned and errno is set to indicate the error.
+        if (currentLineOffset == ERROR)
+        { // the value (off_t) -1 is returned and errno is set to indicate the error.
             perror("Lseek Error: ");
             exit(EXIT_FAILURE);
         }
@@ -71,45 +75,52 @@ int BuildLinesOffsetsTable(int fileDescriptor, long* linesOffsets, int* linesLen
 
 void PrintLine(int fileDescriptor, int lineOffset, int linesLength)
 {
-    lseek(fileDescriptor, lineOffset, 0); // move to requested line
+    lseek(fileDescriptor, lineOffset, SEEK_SET); // move to requested line
 
-    for (int i = 0; i < linesLength; ++i) {
+    for (int i = 0; i < linesLength; ++i)
+    {
         char currentChar;
-        if (read(fileDescriptor, &currentChar, 1) == ERROR) {
+        if (read(fileDescriptor, &currentChar, 1) == ERROR)
+        {
             perror("Reading Error: ");
             exit(EXIT_FAILURE);
         }
 
-        if (write(STDOUT_FILENO, &currentChar, 1) == ERROR) {
+        if (write(STDOUT_FILENO, &currentChar, 1) == ERROR)
+        {
             perror("Writing Error: ");
             exit(EXIT_FAILURE);
         }
     }
 }
 
-/**
- *
- * return: EXIT_SUCCESS, EXIT_FAILURE if errors occured
- */
-int RequestingUserInputLoop(int fileDescriptor, int linesCount, long* linesOffsets, int* linesLengths)
+
+
+ //return: EXIT_SUCCESS, EXIT_FAILURE if errors occured
+
+int RequestingUserInputLoop(int fileDescriptor, int linesCount, long *linesOffsets, int *linesLengths)
 {
     printf("Lines range: [%d, %d]\n", linesCount > 0, linesCount);
-    do {
+    do
+    {
         printf("$ ");
 
         int lineNumber;
         char ending;
 
-        if (scanf("%d%c", &lineNumber, &ending) != 2 || ending != '\n') {
+        if (scanf("%d%c", &lineNumber, &ending) != 2 || ending != '\n')
+        {
             fprintf(stderr, "Format Error\n");
             return EXIT_FAILURE;
         }
 
-        if (lineNumber == END_LINE_NUMBER) {
+        if (lineNumber == END_LINE_NUMBER)
+        {
             return EXIT_SUCCESS;
         }
 
-        if (lineNumber < 0 || lineNumber > linesCount) {
+        if (lineNumber < 0 || lineNumber > linesCount)
+        {
             fprintf(stderr, "Out Of Range Error: lineNumber %d out of range [%d, %d]\n", lineNumber, linesCount > 0, linesCount);
             continue;
         }
@@ -118,23 +129,27 @@ int RequestingUserInputLoop(int fileDescriptor, int linesCount, long* linesOffse
     } while (1);
 }
 
-int main(int argc, char* argv[])
-{
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s [file]\n", argv[0]);
-        return EINVAL;
-    }
 
+int main(int argc, char *argv[])
+{
+    const char* file_name = (argc == 2) ? argv[1] : DEFAULT_FILE_NAME;
+
+    //таблица отступов в файле
     long linesOffsets[LINES_COUNT_MAX + 1];
+    //длины строк для каждой строки в файле
     int linesLengths[LINES_COUNT_MAX + 1];
 
-    int fileDescriptor = open(argv[1], O_RDONLY | O_NDELAY);
+   //файл, на имя которого указывает file_name, открывается для чтения
+   //возвращаемое значение - файловый дескриптор
+    int fileDescriptor = open(file_name, O_RDONLY);
 
-    if (fileDescriptor == ERROR) {
+    if (fileDescriptor == ERROR)
+    {
         perror(argv[0]);
         return EIO;
     }
 
     int linesCount = BuildLinesOffsetsTable(fileDescriptor, linesOffsets, linesLengths);
+
     return RequestingUserInputLoop(fileDescriptor, linesCount, linesOffsets, linesLengths);
 }
