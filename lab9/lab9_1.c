74f427230432380189f5bd8b43def78ffb840d2a
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-03-25 02:08:53 +0700

    Create lab9_1.c

---
+++ lab9/lab9_1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h> // execl
+#include <errno.h>
+
+#define CHILD_ID 0
+#define ERROR -1
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s file_name\n", argv[0]);
+        return EINVAL;
+    }
+
+    pid_t process_id = fork();
+
+    // При неудаче родительскому процессу возвращается -1, дочерний процесс не создается
+    if (process_id == ERROR) {
+        perror("Fork error: ");
+        return EXIT_FAILURE;
+    }
+
+    // При успешном завершении родителю возвращается PID процесса-потомка, а процессу-потомку возвращается 0
+    if (process_id == CHILD_ID) {
+        execl("/bin/cat", "cat", argv[1], NULL);
+        return EXIT_SUCCESS;
+    }
+
+    printf("\nPARENT MSG\n\n");
+
+    return EXIT_SUCCESS;
+}
