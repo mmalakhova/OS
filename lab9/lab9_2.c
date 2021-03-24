99644be8b44c2efe795c7f6eae4750680d25662c
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-03-25 02:09:40 +0700

    Create lab9_2.c

---
+++ lab9/lab9_2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h> // wait
+#include <sys/wait.h> // wait
+#include <unistd.h> // execl
+#include <errno.h>
+
+#define CHILD_ID 0
+#define ERROR -1
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s file_name\n", argv[0]);
+        return EINVAL;
+    }
+
+    pid_t process_id = fork();
+
+    if (process_id == ERROR) {
+        perror("Fork error: ");
+        return EXIT_FAILURE;
+    }
+
+    if (process_id == CHILD_ID) {
+        execl("/bin/cat", "cat", argv[1], NULL);
+        return EXIT_SUCCESS;
+    }
+
+    // Возвращает идентификатор дочернего процесса, который завершил выполнение
+    // 0 означает ожидание любого дочернего процесса, идентификатор группы процессов которого равен идентификатору текущего процесса
+    pid_t ended_process_id = wait(NULL);
+
+    if (ended_process_id == ERROR) {
+        perror("TIMEOUT ERROR: ");
+        return EXIT_FAILURE;
+    }
+
+    printf("\nPARENT MSG\n");
+
+    return EXIT_SUCCESS;
+}
