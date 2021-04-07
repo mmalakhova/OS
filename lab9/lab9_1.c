6d0ae454493948da477b2d2f62eca3d2f3b1d114
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-04-07 20:31:58 +0700

    Update lab9_1.c

--- lab9/lab9_1.c
+++ lab9/lab9_1.c
@@ -7,28 +7,35 @@
 #define CHILD_ID 0
 #define ERROR -1
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s file_name\n", argv[0]);
-        return EINVAL;
-    }
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s file_name\n", argv[0]);
+		return EINVAL;
+	}
 
-    pid_t process_id = fork();
+	pid_t process_id = fork();
 
-    // При неудаче родительскому процессу возвращается -1, дочерний процесс не создается
-    if (process_id == ERROR) {
-        perror("Fork error: ");
-        return EXIT_FAILURE;
-    }
+	// При неудаче родительскому процессу возвращается -1, дочерний процесс не создается
+	// возможные значения errno:
+	// EAGAIN - не может скопировать таблицы страниц родителя и выдерить структуру описания потомка
+	// ENOMEM - ...
+	if (process_id == ERROR)
+	{
+		perror("Fork error: ");
+		return EXIT_FAILURE;
+	}
 
-    // При успешном завершении родителю возвращается PID процесса-потомка, а процессу-потомку возвращается 0
-    if (process_id == CHILD_ID) {
-        execl("/bin/cat", "cat", argv[1], NULL);
-        return EXIT_SUCCESS;
-    }
+	// При успешном завершении родителю возвращается PID процесса-потомка, а процессу-потомку возвращается 0
+	if (process_id == CHILD_ID)
+	{
+		execl("/bin/cat", "cat", argv[1], NULL);
+		perror("Execl error: ");
+		return EXIT_FAILURE;
+	}
 
-    printf("\nPARENT MSG\n\n");
+	printf("\nPARENT MSG\n\n");
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
