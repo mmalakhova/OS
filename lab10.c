7e6a72306cdcc6f0f71f1935e06d4e2d18ac973c
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-04-20 09:32:50 +0700

    Update lab10.c

--- lab10.c
+++ lab10.c
@@ -10,47 +10,53 @@
 #define SUBPROCESS_ERROR 0
 #define NOT_TERMINATED_BY_SIGNAL 0
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    if (argc != 3) {
+    if (argc <= 2)
+    {
         fprintf(stderr, "Usage: %s [executable] <args>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
     pid_t process_id = fork();
 
-    if (process_id == FORK_ERROR) {
+    if (process_id == FORK_ERROR)
+    {
         perror("Fork error");
         return EXIT_FAILURE;
     }
 
-    if (process_id == CHILD_ID) {
+    if (process_id == CHILD_ID)
+    {
         // v (vector). Аргументы командной строки передаются в форме вектора argv[]. Отдельные аргументы адресуются через argv [0], argv [1]... argv [n].
         // Последний аргумент (argv [n]) должен быть указателем NULL;
         // p (path). Обозначенный по имени файл ищется не только в текущем каталоге, но и в каталогах, определенных переменной среды PATH;
         execvp(argv[1], &argv[1]);
-        perror("Exec error");
+        perror("Execvp error");
         return EXIT_FAILURE;
     }
 
     int status;
     pid_t ended_process_id = wait(&status);
 
-    if (ended_process_id == WAIT_ERROR) {
+    if (ended_process_id == WAIT_ERROR)
+    {
         perror("Wait");
         return EXIT_FAILURE;
     }
 
-    if (WIFEXITED(status) != SUBPROCESS_ERROR) {
+    if (WIFSIGNALED(status))
+    {
+        printf("\nChild process %d ended by signal, signal: %d\n\n", ended_process_id, WTERMSIG(status));
+        // WTERMSIG(wstatus) returns the number of the signal that caused the child process to terminate.
+        return EXIT_SUCCESS;
+    }
+    
+    if (WIFEXITED(status))
+    {
         // WIFEXITED(wstatus) returns true if the child terminated normally, that is, by calling exit(3) or _exit(2), or by returning from main().
         printf("\nChild process %d ended by exit, return's code: %d\n\n", ended_process_id, WEXITSTATUS(status));
         // WEXITSTATUS(wstatus) returns the exit status of the child.
-        return EXIT_SUCCESS;
-    }
-
-    if (WIFSIGNALED(status) != NOT_TERMINATED_BY_SIGNAL) {
-        printf("\nChild process %d ended by signal, signal: %d\n\n", ended_process_id, WTERMSIG(status));
-        // WTERMSIG(wstatus) returns the number of the signal that caused the child process to terminate.
         return EXIT_SUCCESS;
     }
 
