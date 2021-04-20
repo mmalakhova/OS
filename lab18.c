09eac5b4e6affd6ccde4dcbfb8eee34681891948
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-04-20 09:08:02 +0700

    Update lab18.c

--- lab18.c
+++ lab18.c
@@ -10,7 +10,7 @@
 #include <unistd.h>
 
 /// struct stat {
-///     dev_t         st_dev;      /* устройство */
+///     dev_t         st_dev;      /*Это поле идентифицирует файловую систему, которая содержит заданный файл*/
 ///     ino_t         st_ino;      /* inode */
 ///     mode_t        st_mode;     /* режим доступа */
 ///     nlink_t       st_nlink;    /* количество жестких ссылок */
@@ -32,12 +32,14 @@
 #define RIGHTS_TYPES_NUM 9
 #define RIGHTS_SYMBS_NUM 3
 #define CUR_DIR "."
+#define STAT_ERROR -1
 
-struct stat get_file_status(char* filename)
+struct stat get_file_status(char *filename)
 {
     struct stat status;
 
-    if (stat(filename, &status) == -1) {
+    if (stat(filename, &status) == STAT_ERROR)
+    {
         perror(filename);
         exit(EXIT_FAILURE);
     }
@@ -45,19 +47,22 @@ struct stat get_file_status(char* filename)
     return status;
 }
 
-char* get_file_access_rights(struct stat* status)
+char *get_file_access_rights(struct stat *status)
 {
-    char rights_symbols[RIGHTS_SYMBS_NUM] = { 'r', 'w', 'x' };
-    int rights_types[RIGHTS_TYPES_NUM] = { S_IRUSR, S_IWUSR, S_IXUSR, S_IRGRP, S_IWGRP, S_IXGRP, S_IROTH, S_IWOTH,
-        S_IXOTH };
-    char* access_rights = malloc(RIGHTS_TYPES_NUM + 1);
+    char rights_symbols[RIGHTS_SYMBS_NUM] = {'r', 'w', 'x'};
+    int rights_types[RIGHTS_TYPES_NUM] = {S_IRUSR, S_IWUSR, S_IXUSR, S_IRGRP, S_IWGRP, S_IXGRP, S_IROTH, S_IWOTH,
+                                          S_IXOTH};
+    char *access_rights = malloc(RIGHTS_TYPES_NUM + 1);
 
-    for (int i = 0; i < RIGHTS_TYPES_NUM; ++i) {
+    for (int i = 0; i < RIGHTS_TYPES_NUM; ++i)
+    {
         access_rights[i] = '-';
     }
 
-    for (int right_index = 0; right_index < RIGHTS_TYPES_NUM; right_index++) {
-        if (status->st_mode & rights_types[right_index]) {
+    for (int right_index = 0; right_index < RIGHTS_TYPES_NUM; right_index++)
+    {
+        if (status->st_mode & rights_types[right_index])
+        {
             access_rights[right_index] = rights_symbols[right_index % RIGHTS_SYMBS_NUM];
         }
     }
@@ -65,15 +70,17 @@ char* get_file_access_rights(struct stat* status)
     return access_rights;
 }
 
-char* get_file_owner(struct stat* status)
+
+char *get_file_owner(struct stat *status)
 {
     /// struct passwd {
     ///         char    *pw_name;       /* имя пользователя */
     ///         /* ... */
     /// };
 
-    struct passwd* owner_info = getpwuid(status->st_uid);
-    if (owner_info == NULL) {
+    struct passwd *owner_info = getpwuid(status->st_uid);
+    if (owner_info == NULL)
+    {
         perror("GET OWNER INFO");
         exit(EXIT_FAILURE);
     }
@@ -81,10 +88,12 @@ char* get_file_owner(struct stat* status)
     return owner_info->pw_name;
 }
 
-char* get_file_group(struct stat* status)
+
+char *get_file_group(struct stat *status)
 {
-    struct group* group_info = getgrgid(status->st_gid);
-    if (group_info == NULL) {
+    struct group *group_info = getgrgid(status->st_gid);
+    if (group_info == NULL)
+    {
         perror("GET OWNER INFO");
         exit(EXIT_FAILURE);
     }
@@ -92,10 +101,11 @@ char* get_file_group(struct stat* status)
     return group_info->gr_name;
 }
 
-char* get_file_timestamp(struct stat* status)
+char *get_file_timestamp(struct stat *status)
 {
-    char* time = ctime(&status->st_mtime);
-    if (time == NULL) {
+    char *time = ctime(&status->st_mtime);
+    if (time == NULL)
+    {
         perror("GET TIME");
         exit(EXIT_FAILURE);
     }
@@ -103,79 +113,87 @@ char* get_file_timestamp(struct stat* status)
     return time;
 }
 
-char* get_file_name(char* filepath)
+char *get_file_name(char *filepath)
 {
     int filename_index = strlen(filepath) - 1;
 
-    while (filepath[filename_index] != '/' && filename_index != 0) {
+    while (filepath[filename_index] != '/' && filename_index != 0)
+    {
         filename_index--;
     }
 
-    if (filename_index != 0) {
+    if (filename_index != 0)
+    {
         filename_index++;
     }
 
     return &filepath[filename_index];
 }
 
-char get_file_type(struct stat* status)
+char get_file_type(struct stat *status)
 {
-    switch (status->st_mode & S_IFMT) {
-    case S_IFREG:
-        return '-';
-    case S_IFDIR:
-        return 'd';
-    default:
-        return '?';
+    switch (status->st_mode & S_IFMT)
+    {
+        case S_IFREG:
+            return '-';
+        case S_IFDIR:
+            return 'd';
+        default:
+            return '?';
     }
 }
 
-int get_file_links_num(struct stat* status)
+
+nlink_t get_file_links_num(struct stat *status)
 {
-    return (int)status->st_nlink;
+   return status->st_nlink;
 }
 
-int get_file_size(struct stat* status)
+off_t get_file_size(struct stat *status)
 {
-    return (int)status->st_size;
+	return status->st_size;   
 }
 
-void print_file_info(char* filename)
+
+void print_file_info(char *filename)
 {
     struct stat status;
-    //  = get_file_status(filename);
 
-    if (stat(filename, &status) == -1) {
+    if (stat(filename, &status) == STAT_ERROR)
+    {
         perror(filename);
         return;
     }
 
-    char* access_rights = get_file_access_rights(&status);
+    char *access_rights = get_file_access_rights(&status);
 
-    printf("%c%s   %d %s %s    %d %.19s %s\n",
+    printf("%c%s   %ld %s %s    %ld %.19s %s\n",
 
-        get_file_type(&status),
-        access_rights,
-        get_file_links_num(&status),
-        get_file_owner(&status),
-        get_file_group(&status),
-        get_file_size(&status),
-        get_file_timestamp(&status),
-        get_file_name(filename));
+           get_file_type(&status),
+           access_rights,
+           get_file_links_num(&status),
+           get_file_owner(&status),
+           get_file_group(&status),
+           get_file_size(&status),
+           get_file_timestamp(&status),
+           get_file_name(filename));
 
     free(access_rights);
 }
 
-int main(int argc, char** argv)
+
+int main(int argc, char **argv)
 {
-    if (argc < 2) {
+    if (argc < 2)
+    {
         print_file_info(CUR_DIR);
         return EXIT_SUCCESS;
     }
 
     int filename_index;
 
-    for (filename_index = 1; filename_index < argc; filename_index++) {
+    for (filename_index = 1; filename_index < argc; filename_index++)
+    {
         print_file_info(argv[filename_index]);
     }
 
