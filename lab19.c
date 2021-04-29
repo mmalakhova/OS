8a031daaff790b581736d8ec350bacb1d75451ae
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-04-29 22:33:27 +0700

    Update lab19.c

--- lab19.c
+++ lab19.c
@@ -6,6 +6,37 @@
 #include <glob.h>
 
 #define GLOB_SUCCESS 0
+#define NOT_END_OF_INPUT 1
+
+void read_template(char* mask)
+{
+    int length = 0;
+
+    while (NOT_END_OF_INPUT) {
+        if (length > PATH_MAX) {
+            fprintf(stderr, "Too long template. Max file name length is %d\n", PATH_MAX);
+            exit(EXIT_FAILURE);
+        }
+
+        char read_symb = (char)fgetc(stdin);
+
+        if (read_symb == EOF || read_symb == '\n') {
+            break;
+        }
+
+        if (read_symb == '/') {
+            fprintf(stderr, "'/' must not occur in the template.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        mask[length++] = read_symb;
+    }
+
+    if (length == 0) {
+        mask[length++] = '*';
+    }
+    mask[length] = '\0';
+}
 
 int errFunc(const char* epath, int errno)
 {
@@ -25,6 +56,7 @@ void print_matched_files(char* mask)
     // Другие возможные возвращаемые значения: GLOB_NOSPACE, GLOB_ABORTED, GLOB_NOMATCH
     if (glob_status != GLOB_SUCCESS) {
         perror("GLOB");
+        globfree(&glob_results);
         exit(EXIT_FAILURE);
     }
 
@@ -32,27 +64,22 @@ void print_matched_files(char* mask)
     // Указатель, который находится непосредственно за последним в списке имен, равен NULL.
     int matching_index;
     for (matching_index = 0; matching_index < glob_results.gl_pathc; ++matching_index) {
-        printf("%s ", glob_results.gl_pathv[matching_index]);
-    }
-
-    if (matching_index) {
-        printf("\n");
+        printf("%s\n", glob_results.gl_pathv[matching_index]);
     }
 
     globfree(&glob_results);
 }
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <args>\n", argv[0]);
+    if (argc > 1) {
+        fprintf(stderr, "Usage: %s \n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    int i;
-    for (i = 1; i < argc; ++i) {
-        print_matched_files(argv[i]);
-    }
+    char mask[PATH_MAX + 1];
+    read_template(mask);
+    print_matched_files(mask);
 
     return EXIT_SUCCESS;
 }
