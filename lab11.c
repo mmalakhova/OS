46de6117dbda1644379a18956141975bff6272ae
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-04-29 17:15:42 +0700

    Update lab11.c

--- lab11.c
+++ lab11.c
@@ -6,8 +6,9 @@
 
 extern char** environ;
 
-int execvpe(char* file, char* argv[], char* envp[]) {
-    if(file == NULL) {
+int execvpe(char* file, char* argv[], char* envp[])
+{
+    if (file == NULL) {
         perror("NULL pointer to file");
         return EXIT_FAILURE;
     }
@@ -15,25 +16,26 @@ int execvpe(char* file, char* argv[], char* envp[]) {
     environ = envp;
 
     execvp(file, argv);
-	environ = savedEnviron;
-	perror("exec error");
-	return EXIT_FAILURE;
+    environ = savedEnviron;
+    perror("exec error");
+    return EXIT_FAILURE;
 }
 
-int main(int argc, char* argv[], char* envp[]) {
+int main(int argc, char* argv[], char* envp[])
+{
 
-    if(argc < 2) {
+    if (argc < 2) {
         fprintf(stderr, "Usage: %s [executable] <args>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    if(envp == NULL) {
+    if (envp == NULL) {
         perror("Environment is not set");
     }
 
-    if(putenv("TZ=PST8PDT") != PUTENV_SUCCESS){
+    if (putenv("TZ=PST8PDT") != PUTENV_SUCCESS) {
         perror("putenv error");
-	return EXIT_FAILURE;
+        return EXIT_FAILURE;
     }
 
     execvpe(argv[1], &argv[1], envp);
