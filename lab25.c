b87c12ca3a812dd253d4b34bd0b089af807dcb88
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-05-05 03:42:14 +0700

    Update lab25.c

--- lab25.c
+++ lab25.c
@@ -13,23 +13,10 @@
 #define NO_INPUT_DATA 0
 #define HAVE_INPUT_DATA 1
 #define HAVE_DATA_FROM_PIPE 1
-#define HAVE_WORKING_SUBPROCESS 1
 #define ERROR -1
 
 int initialize_writing_proc(int fildes[])
 {
-    pid_t pid = fork();
-    if (pid == ERROR) {
-        perror("Making subprocess for writing");
-        exit(EXIT_FAILURE);
-    }
-
-    if (pid != CHILD_ID) {
-        return EXIT_SUCCESS;
-    }
-
-    close(fildes[READ]);
-
     while (HAVE_INPUT_DATA) {
         char symbol;
         int bytes_read = read(STDIN_FILENO, &symbol, sizeof(char));
@@ -39,36 +26,22 @@ int initialize_writing_proc(int fildes[])
 
         if (bytes_read == ERROR) {
             perror("Reading from stdin");
-            exit(EXIT_FAILURE);
+            return EXIT_FAILURE;
         }
 
         int bytes_wrote = write(fildes[WRITE], &symbol, sizeof(char));
         if (bytes_wrote == ERROR) {
             perror("Writing to pipe");
-            exit(EXIT_FAILURE);
+            return EXIT_FAILURE;
         }
     }
 
-    close(fildes[WRITE]);
-    exit(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
 
 int initialize_reading_proc(int fildes[])
 {
     int bytes_wrote = 0;
-
-    pid_t pid = fork();
-    if (pid == ERROR) {
-        perror("Making subprocess for reading");
-        exit(EXIT_FAILURE);
-    }
-
-    if (pid != CHILD_ID) {
-        return EXIT_SUCCESS;
-    }
-
-    close(fildes[WRITE]);
-
     while (HAVE_DATA_FROM_PIPE) {
         char symbol;
         int bytes_read = read(fildes[READ], &symbol, sizeof(char));
@@ -78,7 +51,7 @@ int initialize_reading_proc(int fildes[])
 
         if (bytes_read == ERROR) {
             perror("Reading from pipe");
-            exit(EXIT_FAILURE);
+            return EXIT_FAILURE;
         }
 
         symbol = (char)toupper(symbol);
@@ -86,35 +59,11 @@ int initialize_reading_proc(int fildes[])
         bytes_wrote = write(STDOUT_FILENO, &symbol, sizeof(char));
         if (bytes_wrote == ERROR) {
             perror("Writing to stdout");
-            exit(EXIT_FAILURE);
+            return EXIT_FAILURE;
         }
     }
-
     write(STDOUT_FILENO, "\n", sizeof(char));
-    close(fildes[READ]);
-    exit(EXIT_SUCCESS);
-}
-
-void wait_subprocesses()
-{
-    while (HAVE_WORKING_SUBPROCESS) {
-        int status;
-        int ended_process_id = wait(&status);
-
-        if (ended_process_id == ERROR && errno == ECHILD) {
-            break;
-        }
-
-        if (WIFEXITED(status) && WEXITSTATUS(status) != EXIT_SUCCESS) {
-            fprintf(stderr, "Subprocess %d corrupt an error\n", ended_process_id);
-            exit(EXIT_FAILURE);
-        }
-
-        if (WIFSIGNALED(status)) {
-            fprintf(stderr, "Subprocess %d corrupt an error\n", ended_process_id);
-            exit(EXIT_FAILURE);
-        }
-    }
+    return EXIT_SUCCESS;
 }
 
 int main(int argc, char** argv)
@@ -127,13 +76,42 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    initialize_writing_proc(fildes);
-    initialize_reading_proc(fildes);
+    pid_t writing_subprocess = fork();
+    if (writing_subprocess == ERROR) {
+        close(fildes[READ]);
+        close(fildes[WRITE]);
+        return EXIT_FAILURE;
+    }
+    if (writing_subprocess == CHILD_ID) {
+        close(fildes[READ]);
+        int status = initialize_writing_proc(fildes);
+        close(fildes[WRITE]);
+        return status;
+    }
+
+    pid_t reading_subprocess = fork();
+    if (reading_subprocess == ERROR) {
+        close(fildes[READ]);
+        close(fildes[WRITE]);
+        return EXIT_FAILURE;
+    }
+    if (reading_subprocess == CHILD_ID) {
+        close(fildes[WRITE]);
+        int status = initialize_reading_proc(fildes);
+        close(fildes[READ]);
+        return status;
+    }
 
     close(fildes[READ]);
     close(fildes[WRITE]);
 
-    wait_subprocesses();
-
+    if (waitpid(writing_subprocess, NULL, 0) == (pid_t)ERROR) {
+        perror("WAITPID ERROR");
+        return ERROR;
+    }
+    if (waitpid(reading_subprocess, NULL, 0) == (pid_t)ERROR) {
+        perror("WAITPID ERROR");
+        return ERROR;
+    }
     return EXIT_SUCCESS;
 }
